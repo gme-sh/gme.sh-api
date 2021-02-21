d963fb1353dc352509e2085dd52c504bd37d02df
darmiel <71837281+darmiel@users.noreply.github.com>
2021-02-21 12:21:36 +0100

    Updated README.md
    Signed-off-by: darmiel <71837281+darmiel@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-[![forthebadge](https://forthebadge.com/images/badges/made-with-go.svg)](https://forthebadge.com)  
+<img align="right" src="https://forthebadge.com/images/badges/made-with-go.svg"></img>
+
+[![Go Report Card](https://goreportcard.com/badge/github.com/gme-sh/gme.sh-api)](https://goreportcard.com/report/github.com/gme-sh/gme.sh-api)
 ![Build Go](https://github.com/full-stack-gods/gme.sh-api/workflows/Build%20Go/badge.svg)
 ![Build Docker](https://github.com/full-stack-gods/gme.sh-api/workflows/Build%20Docker/badge.svg)
 
 # GMEshortener
 $GME go brrrrr
 
-![brr](https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F2118%2F1625%2Fproducts%2F000786a-6_1000x1000.png%3Fv%3D1586266264&f=1&nofb=1)
-
-\u2588\u2588
+> Allan, please add details!
--- internal/gme-sh/web/web_server.go
+++ internal/gme-sh/web/web_server.go
@@ -18,12 +18,29 @@ type WebServer struct {
 func (ws *WebServer) Start() {
 	router := mux.NewRouter()
 
-	router.HandleFunc("/gme-sh-block", ws.handleGMEBlock)
-	router.HandleFunc("/api/v1/create", ws.handleApiV1Create).Methods("POST")
-	router.HandleFunc("/api/v1/stats/{id}", ws.handleApiV1Stats).Methods("GET")
-	router.HandleFunc("/api/v1/heartbeat", ws.handleApiV1Heartbeat).Methods("GET")
-	router.HandleFunc("/api/v1/{id}/{secret64}", ws.handleApiV1Delete).Methods("DELETE")
-	router.HandleFunc("/{id}", ws.handleRedirect)
+	// GET /gme-sh-block
+	// Used to check if a web page blocks gme.sh (e.g. for loop prevention).
+	router.HandleFunc("/gme-sh-block", ws.handleGMEBlock).Methods(http.MethodGet)
+
+	// GET /heartbeat
+	// This page returns the status 200 if the backend is running. otherwise a 5xx error
+	router.HandleFunc("/heartbeat", ws.handleApiV1Heartbeat).Methods(http.MethodGet)
+
+	// POST /create
+	// Used to create new short URLs
+	router.HandleFunc("/create", ws.handleApiV1Create).Methods(http.MethodPost)
+
+	// DELETE /{id}/{secret}
+	// Used to delete short URLs
+	router.HandleFunc("/{id}/{secret64}", ws.handleApiV1Delete).Methods(http.MethodDelete)
+
+	// GET /stats/{id}
+	// Used to retrieve stats for a short url
+	router.HandleFunc("/stats/{id}", ws.handleApiV1Stats).Methods(http.MethodGet)
+
+	// GET /{id}
+	// Used for redirection to long url
+	router.HandleFunc("/{id}", ws.handleRedirect).Methods(http.MethodGet)
 
 	log.Println("🌎 Binding", ws.config.WebServer.Addr, "...")
 	if err := http.ListenAndServe(ws.config.WebServer.Addr, router); err != nil {
